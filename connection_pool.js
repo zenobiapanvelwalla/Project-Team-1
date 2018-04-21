10b3dc40f28fd774df9171c1728b64a52b773fd9
AishwaryaSaxena <aishwarya.saxena@sjsu.edu>
2018-04-21 11:30:58 -0700

    no message

--- connection_pool.js
+++ connection_pool.js
@@ -5,7 +5,7 @@ function connectionPool() {
         connectionLimit: 100, //important
         host: "localhost",
         user: "root",
-        password: "283229ansz",
+        password: "root",
         database: "fms"
     });
     pool.getConnection(function(err, connection) {
