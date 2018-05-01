f8e3d2c4fb9bfff5baebd4197ff7b2204ac4aca9
AishwaryaSaxena <aishwarya.saxena@sjsu.edu>
2018-05-01 11:39:27 -0700

    **Added new Chart ****

--- routes/chartData.js
+++ routes/chartData.js
@@ -20,6 +20,24 @@ router.get('/', function(req, res, next) {
     });
     
 });
+router.get('/status', function(req, res, next) {
+    var user_id = localStorage.getItem('user_id');
+    console.log('user_id');
+    var sql = `select status,count(status) as count from lead_tasks join leads on lead_tasks.lead_id=leads.id where user_id=${user_id} group by status;`;
+    var query = con.query(sql,(error, results) => {
+        if(error) throw error;
+        console.log(results);
+        if(results != undefined)
+        {
+            res.send(results);
+        }
+            else{
+                res.send('Invalid');
+            }
+    });
+    
+});
+
 
 
 
