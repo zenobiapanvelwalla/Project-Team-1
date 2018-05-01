7d0c2e877adc404bbd7ce3ee15ca882ce5aa1f42
zenobia panvelwalla <zenobiapanvelwalla@gmail.com>
2018-05-01 10:00:41 -0700

    removed signout bug

--- routes/signout.js
+++ routes/signout.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var localStorage = require('node-localstorage');
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+  }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
     req.session.destroy();
-    localStorage.clear();
+    //localStorage.clear();
 
-    res.render("index");
+    res.render("index",{error:null});
 });
 
 
